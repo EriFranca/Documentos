ff397803f4498ed6c1f24b1af90e419ac08a580e
Eri França <ecfs1988@hotmail.com>
2023-10-11 20:27:58 -0300

    linhas gerais para acesso ao ZFIR002

--- pyautoguiteste.py
+++ pyautoguiteste.py
@@ -1,12 +1,43 @@
 import pyautogui
 import time
 
-pyautogui.alert("Alerta")
-pyautogui.PAUSE = 0.5
-pyautogui.press('winleft')
-pyautogui.write('chrome')
+import pyautogui
+import time
+
+# 1 - Verificar se existe uma janela do SAP aberta
+# Implemente a lógica de verificação da janela do SAP aberta de acordo com suas necessidades
+
+# 2 - Acessar a transação /OZFIR002
+# Pressionar a combinação de teclas para acessar a transação
+pyautogui.hotkey('ctrl', '/')
+
+# Esperar um tempo para o SAP abrir
+time.sleep(5)
+
+# Digitar a transação /OZFIR002
+pyautogui.write('/OZFIR002')
 pyautogui.press('enter')
+
+# Esperar um tempo para a transação abrir
+time.sleep(5)
+
+# 3 - Configurar data e loja nos locais adequados
+# Implemente a lógica para configurar data e loja de acordo com suas necessidades
+
+# 4 - Exportar o relatório gerado
+# Implemente a lógica para exportar o relatório de acordo com suas necessidades
+
+# 5 - Selecionar o arquivo excel gerado
+# Implemente a lógica para selecionar o arquivo excel de acordo com suas necessidades
+
+# 6 - Copiar as informações na coluna G
+# Implemente a lógica para copiar as informações da coluna G de acordo com suas necessidades
+
+# 7 - Abrir o Chrome na página "https://fiscal.com.br"
+pyautogui.hotkey('winleft', 'r')  # Pressionar a tecla Windows + R para abrir Executar
 time.sleep(1)
-pyautogui.write("https://guiadosquadrinhos.com.br")
+pyautogui.write('chrome https://fiscal.com.br')  # Digitar o comando para abrir o Chrome com a URL
 pyautogui.press('enter')
-#teste
+
+# Esperar um tempo para o Chrome abrir e a página carregar
+time.sleep(10)
